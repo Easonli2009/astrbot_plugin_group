0801fbb8c5a3278efbb2f8f1c731a56ee9a8532d
Easonli2009 <eason_lyz@qq.com>
2025-01-26 21:42:35 +0800

    test

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from astrbot.api.all import *
 
-@register("helloworld", "Lyz09", "让BOT成为活跃于群内的QQ人", "1.0.1")
+@register("helloworld", "Lyz09", "我的插件", "1.0.2")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
     
     @event_message_type(EventMessageType.PRIVATE_MESSAGE) #过滤私聊消息
     async def on_private_message(self, event: AstrMessageEvent):
-        yield event.plain_result("收到了一条私聊消息。")
+        yield event.plain_result("我不理你！😤")
