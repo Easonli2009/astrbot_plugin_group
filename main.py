9d80e06f8b08a31cbce9ecb01b59492ec9e49561
Easonli2009 <eason_lyz@qq.com>
2025-01-27 22:05:15 +0800

    test

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
-    @event_message_type(EventMessageType.GROUP_MESSAGE) # 注册一个过滤器，参见下文。
+    @command("test")
+    async def test(self, event: AstrMessageEvent):
+        provider = self.context.get_using_provider()
+        if provider:
+            response = await provider.text_chat("你好", session_id=event.session_id)
+            print(response.completion_text) # LLM 返回的结果
+
+    @event_message_type(EventMessageType.GROUP_MESSAGE) # 注册一个过滤器
     async def on_message(self, event: AstrMessageEvent):
         print("#Debug Message: ")
         print(event.message_obj.raw_message) # 打印消息内容
@@ -18,7 +25,7 @@ class MyPlugin(Star):
             dc[group_id]=0
         dc[group_id] = int(dc.get(group_id))+1
         print(dc[group_id])
-        if dc[group_id]>=6:
+        if dc[group_id]>=2:
             dc[group_id]=0
             provider = self.context.get_using_provider()
             if provider:
