32320c63eaa90542b8f06e92fcd1c7f754f7bec9
Easonli2009 <eason_lyz@qq.com>
2025-01-27 20:52:57 +0800

    test

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ class MyPlugin(Star):
 
     @event_message_type(EventMessageType.ALL) # 注册一个过滤器，参见下文。
     async def on_message(self, event: AstrMessageEvent):
-        print("Debug Message:"+event.message_obj.raw_message) # 打印消息内容
+        print("#Debug Message: ")
+        print(event.message_obj.raw_message) # 打印消息内容
