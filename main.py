2dd58e1b9e085afc65de98c2f10839ecd29fb9ae
Easonli2009 <eason_lyz@qq.com>
2025-01-28 18:28:27 +0800

    test

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ class MyPlugin(Star):
         print("#sender.id=" + str(event.message_obj.sender.user_id))
         print("#sender.nickname=" + str(event.message_obj.sender.nickname))
         print("#time=" + str(event.message_obj.timestamp))
+        real_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(event.message_obj.timestamp))
+        print("当前时间：", real_time)
         print("#message=" + str(event.message_obj.message))
         print("#message_real=" + str(event.get_message_outline()))
         global dc
@@ -38,16 +40,17 @@ class MyPlugin(Star):
         if group_id not in his:
             his[group_id] = chat_history()
         dc[group_id] = int(dc.get(group_id)) - 1
-        his[group_id].add("["+str(event.message_obj.sender.nickname)+"(QQid:"+str(event.message_obj.sender.user_id)+")]: "+str(event.get_message_outline()))
-        print("add message:"+"["+str(event.message_obj.sender.nickname)+"(QQid:"+str(event.message_obj.sender.user_id)+")]: "+str(event.get_message_outline()))
+        his[group_id].add("["+str(event.message_obj.sender.nickname)+"(id:"+str(event.message_obj.sender.user_id)+")]: "+str(event.get_message_outline()))
+        print("add message:"+"["+str(event.message_obj.sender.nickname)+"(id:"+str(event.message_obj.sender.user_id)+")]: "+str(event.get_message_outline()))
         print("all message:"+his[group_id].get_all())
         print(dc[group_id])
         if dc[group_id] <= 0:
             dc[group_id] = random.randint(2,7)
             provider = self.context.get_using_provider()
             if provider:
-                prompt_en = "You are currently in a chat room. Based on the chat history, identify the topic that the chat room is currently focused on and respond to that topic with your own answer. Note: You do not need to directly answer the topic you identified. Your response should blend in with the atmosphere of the chat room and should be as conversational as possible. Do not add any embellishments to your answer."
                 prompt_empty = " "
+                # prom = his[group_id].get_all()
+                # prom = prom + "以上是你所在的群聊的历史聊天记录，根据次"
                 response = await provider.text_chat(prompt = prompt_empty, session_id = event.session_id)
                 print(response.completion_text) # LLM 返回的结果
                 yield event.plain_result(response.completion_text) # 发送一条纯文本消息
